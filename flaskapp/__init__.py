61782233755a4fe68c7c4de72897ee5db6d367f1
Bengansomfan <bengan92@gmail.com>
2018-12-19 21:58:41 +0100

    login and logout functionality

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 # Configuration
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'bc8d08cddabdb4fa5b59fd339e4cdafe'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
+db = SQLAlchemy(app)    # database object
+bcrypt = Bcrypt(app)    # password hashing
+login_manager = LoginManager(app)
 
 from flaskapp import routes
--- flaskapp/forms.py
+++ flaskapp/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
-
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskapp.models import User
 
 class RegistrationForm(FlaskForm):
     # We want form fields which needs to be imported
@@ -15,6 +15,15 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up!')
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('username is taken.')
+
+    def validate_email(self, email):
+        email = User.query.filter_by(email=email.data).first()
+        if email:
+            raise ValidationError('email is in use.')
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
