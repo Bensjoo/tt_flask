c25d7a9a0e5aca9c745240d83cc94a0f7f67fdd5
Bengansomfan <bengan92@gmail.com>
2018-12-22 15:36:06 +0100

    login functionality and profile pictures

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -10,5 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)    # database object
 bcrypt = Bcrypt(app)    # password hashing
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskapp import routes
--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,8 +1,11 @@
-from flask import render_template, url_for, flash, redirect
+import os
+import secrets
+from PIL import Image
+from flask import render_template, url_for, flash, redirect, request
 from flaskapp import app, db, bcrypt
 from flaskapp.models import User, Post
-from flaskapp.forms import RegistrationForm, LoginForm
-from flask_login import login_user, current_user, logout_user
+from flaskapp.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
     {
@@ -18,6 +21,7 @@ posts = [
         'date_posted' : '21 december, 2018'
     }
 ]
+
 
 @app.route("/")
 @app.route("/home")
@@ -51,27 +55,72 @@ def register():
 
 @app.route("/login", methods=['GET','POST'])
 def login():
+    # route to home if user is already logged in
     if current_user.is_authenticated:
         return redirect(url_for('home'))
 
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
+
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            flash(f'Login successful!', 'success')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+
         else:
             flash('Login unsuccessful.', 'danger')
+
     return render_template('login.html', title='Log in', form=form)
+
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(nbytes=8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
+
+@app.route("/account", methods=['GET','POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+            # TODO: Cleanup old pics from FS
+            
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Account has been updated!','success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    image_file = url_for('static', filename=('profile_pics/' + current_user.image_file))
+    return render_template('account.html', title='Account',
+                           image_file=image_file, form=form)
+
 
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for('home'))
 
+
 @app.route("/employees")
 def employees():
     return "<h1>Employees Page</h1>"
+
 
 @app.route("/customers")
 def customers():
