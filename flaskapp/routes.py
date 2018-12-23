6285ab6f727aad687e44842a7f61b32f58551a61
Bengansomfan <bengan92@gmail.com>
2018-12-23 13:22:41 +0100

    add/uppdate/delete post functionality

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,31 +1,17 @@
 import os
 import secrets
 from PIL import Image
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from flaskapp import app, db, bcrypt
 from flaskapp.models import User, Post
-from flaskapp.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskapp.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flask_login import login_user, current_user, logout_user, login_required
-
-posts = [
-    {
-        'author' : 'Bengt Sjögren',
-        'title' : 'Blog Post 1',
-        'content' : 'First post!',
-        'date_posted' : '20 december, 2018'
-    },
-    {
-        'author' : 'Jane Doe',
-        'title' : 'Blog Post 2',
-        'content' : 'Second post! Nice!',
-        'date_posted' : '21 december, 2018'
-    }
-]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
+    posts = Post.query.all()
     return render_template('home.html', posts=posts, title='Home')
 
 
@@ -96,7 +82,7 @@ def account():
             picture_file = save_picture(form.picture.data)
             current_user.image_file = picture_file
             # TODO: Cleanup old pics from FS
-            
+
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
@@ -116,6 +102,59 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+
+@app.route("/post/new", methods=['GET','POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data,
+                    author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!','success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post',
+                           form=form, legend='Create New Post')
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+
+@app.route("/post/<int:post_id>/update", methods=['GET','POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post was updated','success')
+        return redirect(url_for('post',post_id=post.id))
+
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post',
+                           form=form, legend='Update Post')
+
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post was deleted','success')
+    return redirect(url_for('home'))
 
 @app.route("/employees")
 def employees():
