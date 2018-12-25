8aaa7b9730dd1906da9c24b5acd4bb6325dea839
Bengansomfan <bengan92@gmail.com>
2018-12-25 12:41:40 +0100

    restructured app into packages and implementation using blueprints and config file.

--- flaskapp/users/forms.py
+++ flaskapp/users/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_login import current_user
 from flaskapp.models import User
 
 class RegistrationForm(FlaskForm):
@@ -54,11 +54,6 @@ class UpdateAccountForm(FlaskForm):
             email = User.query.filter_by(email=email.data).first()
             if email:
                 raise ValidationError('email is in use.')
-
-class PostForm(FlaskForm):
-    title = StringField('title',validators=[DataRequired()])
-    content = TextAreaField('content',validators=[DataRequired()])
-    submit = SubmitField('Post')
 
 
 class RequestResetForm(FlaskForm):
